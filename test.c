68dbe23f6d961e7d6a75b9a4a192995c5f43f991
Luke Richard Stanghelle <kboxkbox1234@gmail.com>
2024-03-07 23:15:38 +0100

    added an example

--- test.c
+++ test.c
@@ -6,16 +6,29 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "test window");
 
     SetTargetFPS(60);
 
+    Vector2 box1 = {100, 100};
+    Vector2 box1size = {100, 100};
+    struct point mouse; 
+
+    struct colideBox hitbox = {{box1.x, box1.y}, {box1.x+100, box1.y+100}};
     while (!WindowShouldClose())
     {
         BeginDrawing();
+            
+            mouse.x = GetMouseX();
+            mouse.y = GetMouseY();
 
             ClearBackground(RAYWHITE);
 
+            if(doesBoxAndPointColide(&hitbox, &mouse) == 1){
+                DrawRectangleV(box1, box1size, RED);
+            }
+            else DrawRectangleV(box1, box1size, GREEN);
+      
             
 
         EndDrawing();
